7edcf93b20881f7b3bfc39c5dfafd2bfdedfe6e7
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-19 18:13:51 +0700

    [UPDATE] first methods

--- SLRMapper.py
+++ SLRMapper.py
@@ -1,9 +1,20 @@
 class SLRMapper:
     def __init__(self):
-        pass
+        self.terminal = {}
+        self.nonterminal = {}
+        self.grammar = {}
 
-    def first(self):
-        pass
+    #not all case (no 'or' case) need grammar loop
+    def first(self, grammar):
+        for char in grammar:
+            if(char in self.terminal):
+                return char
+            elif(char in self.nonterminal):
+                return first(self.grammar[char])
     
-    def follow(self):
+    
+    def follow(self, grammar):
         pass
+        
+
+
