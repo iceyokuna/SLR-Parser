5c4c4526f30724efd76f2c2f427c179ff2651d28
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-20 19:52:25 +0700

    [TEMPLATE] grammar class

---
+++ Grammar.py
@@ -0,0 +1,15 @@
+class Grammar:
+    def __init__(self):
+        self.start = ''
+        self.nonterminal = set()
+        self.terminal = set()
+        self.rule = {}
+
+    def setup(self):
+        #read txt file
+        #construct attr
+        pass
+
+    def getFirst(self, rule_name):
+        #get first of rule_name
+        
