cb81ab1f39b27ab3f26bf4fb891140aa926eb26e
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-21 03:36:02 +0700

    [SET UP] states construction

--- State.py
+++ State.py
@@ -1,3 +1,22 @@
 class State:
     def __init__(self):
+        self.state_name = ""
+        self.kernal = []
+        self.closure = []
+
+    def setUp(self, grammar):
         pass
+
+    def setClosure(self):
+        pass
+
+    def getExpandable(self):
+        return #list
+
+    def isSameKernal(self, other_kernal):
+        return #bool
+
+    def __str__(self):
+        return ""
+    
+        
--- StateManager.py
+++ StateManager.py
@@ -1,5 +1,18 @@
 from Grammar import Grammar
+from State import State
 
 class StateManager:
-    def __init__(self):
-        pass
+    def __init__(self , grammar):
+        self.grammar = grammar
+        self.state_list = []
+        self.transition = {}
+        self.start = '0'
+        self.state_count = 0
+        self.unexpanded_list = []
+
+    def setUp(self):
+        grammar_list = self.grammar.getRuleList()
+        startState = State()
+
+    def __str__(self):
+        return ""
