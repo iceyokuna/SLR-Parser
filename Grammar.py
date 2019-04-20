42d426ba02e61ee13102d9cee4e689f01de683d7
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-20 20:30:06 +0700

    [UPDATE] done parsing file

--- Grammar.py
+++ Grammar.py
@@ -13,13 +13,16 @@ class Grammar:
         grammar_file = open("grammar.txt")
         line_count = 0
         for line in grammar_file:
+            line = line.strip('\n')
             if(line_count == 0):
                 self.start = line.split(' ')[0]
                 self.nonterminal = set(line.split(' '))
             elif(line_count == 1):
                 self.terminal = set(line.split(' '))
             else:
-                self.rule.append(line.split(''))
+                rule = Rule()
+                rule.setRule(line.split(' '))
+                self.rule.append(rule)
             line_count += 1
 
     def getFirst(self, rule_name):
@@ -27,4 +30,17 @@ class Grammar:
         pass
 
     def __str__(self):
-        pass
+        temp_str = "Nonterminal\n"
+        temp_str += str(self.terminal) + '\n\n'    
+        temp_str += "terminal\n"
+        temp_str += str(self.nonterminal) + '\n\n'
+        temp_str += "start symbol\n"
+        temp_str += self.start + '\n\n'
+        temp_str += "Rule\n"
+        for rule in self.rule:
+            temp_str += str(rule) + '\n'
+        return temp_str
+
+g = Grammar()
+g.setup()
+print(g)
