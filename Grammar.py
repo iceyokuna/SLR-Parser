c0babd76e5b7bf41ffcebf9b92773c4270881ce6
Iceyo Kuna <iceyokuna@hotmail.com>
2019-04-20 20:10:57 +0700

    [UPDATE] set up grammar

--- Grammar.py
+++ Grammar.py
@@ -1,15 +1,30 @@
+from Rule import Rule
+
 class Grammar:
     def __init__(self):
         self.start = ''
         self.nonterminal = set()
         self.terminal = set()
-        self.rule = {}
+        self.rule = []
 
     def setup(self):
         #read txt file
         #construct attr
-        pass
+        grammar_file = open("grammar.txt")
+        line_count = 0
+        for line in grammar_file:
+            if(line_count == 0):
+                self.start = line.split(' ')[0]
+                self.nonterminal = set(line.split(' '))
+            elif(line_count == 1):
+                self.terminal = set(line.split(' '))
+            else:
+                self.rule.append(line.split(''))
+            line_count += 1
 
     def getFirst(self, rule_name):
         #get first of rule_name
-        
+        pass
+
+    def __str__(self):
+        pass
